b5a9e72c6bed8bac87d581bf3b7a4f0f1383e151
Lucene Nguyen <manhcuongmvc@gmail.com>
2025-06-17 23:56:38 +0700

    21. Merge Two Sorted Lists

---
+++ Linked List/21.MergeTwoSortedLists.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    const values: number[] = [];
+
+    while (list1) {
+        values.push(list1.val);
+        list1 = list1.next;
+    }
+    while (list2) {
+        values.push(list2.val);
+        list2 = list2.next;
+    }
+
+    const valuesSorted = values.sort((a, b) => a - b);
+    let listNoteSorted = valuesSorted.length ? new ListNode(valuesSorted[0]): null;
+    const head = listNoteSorted;
+    for (let i = 1; i < valuesSorted.length; i++) {
+        listNoteSorted!.next =  new ListNode(valuesSorted[i]);
+        listNoteSorted = listNoteSorted!.next
+    }
+
+    return head;
+};
