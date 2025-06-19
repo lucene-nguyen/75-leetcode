f1bb259c22c4c55b3d37e5896d9b3dc50015b253
Lucene Nguyen <manhcuongmvc@gmail.com>
2025-06-19 23:28:25 +0700

    21. Merge Two Sorted Lists

--- Linked List/21.MergeTwoSortedLists.ts
+++ Linked List/21.MergeTwoSortedLists.ts
@@ -10,25 +10,48 @@
  * }
  */
 
+// mine
+// function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+//     const values: number[] = [];
+
+//     while (list1) {
+//         values.push(list1.val);
+//         list1 = list1.next;
+//     }
+//     while (list2) {
+//         values.push(list2.val);
+//         list2 = list2.next;
+//     }
+
+//     const valuesSorted = values.sort((a, b) => a - b);
+//     let listNoteSorted = valuesSorted.length ? new ListNode(valuesSorted[0]): null;
+//     const head = listNoteSorted;
+//     for (let i = 1; i < valuesSorted.length; i++) {
+//         listNoteSorted!.next =  new ListNode(valuesSorted[i]);
+//         listNoteSorted = listNoteSorted!.next
+//     }
+
+//     return head;
+// };
+
+
+// open ai
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    const values: number[] = [];
+    let current: ListNode = new ListNode(0);
+    const head = current;
 
-    while (list1) {
-        values.push(list1.val);
-        list1 = list1.next;
-    }
-    while (list2) {
-        values.push(list2.val);
-        list2 = list2.next;
-    }
+    while (list1 && list2) {
 
-    const valuesSorted = values.sort((a, b) => a - b);
-    let listNoteSorted = valuesSorted.length ? new ListNode(valuesSorted[0]): null;
-    const head = listNoteSorted;
-    for (let i = 1; i < valuesSorted.length; i++) {
-        listNoteSorted!.next =  new ListNode(valuesSorted[i]);
-        listNoteSorted = listNoteSorted!.next
+        if (list1.val < list2.val) {
+            current.next = list1;
+            list1 = list1.next
+        } else {
+            current.next = list2;
+            list2 = list2.next
+        }
+        current = current.next;
     }
 
-    return head;
+    current.next = list1 || list2;
+    return head.next;
 };
